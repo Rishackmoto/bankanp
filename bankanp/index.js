64caae9e7414b6a6315d4d88703b1670bbd95e74
Rishackmoto <rishackmoto@gmail.com>
2025-08-15 06:12:51 +0900

    Add start script for Railway

--- bankanp/index.js
+++ bankanp/index.js
@@ -46,3 +46,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('API Bank ANP aktif 🚀');
+});
